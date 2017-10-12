76da6b667e03d8ff9ae99f212922a77e573d53a5
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 11:45:16 +0200

    Update tempon_ansewer_ele.java

--- src/java/Data_encryption/tempon_ansewer_ele.java
+++ src/java/Data_encryption/tempon_ansewer_ele.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package data_encryption;
 
-/**
- *
- * @author smahboub
- */
+
 public class tempon_ansewer_ele {
     public String dataID;
     public double PeriScoreinf;
