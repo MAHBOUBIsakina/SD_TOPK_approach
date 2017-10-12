b3ffec00882a5f32490a3c0c8f9192a4255dc464
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 11:47:27 +0200

    Update DataItem.java

--- src/java/Distributed_topk_query_processing/DataItem.java
+++ src/java/Distributed_topk_query_processing/DataItem.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package distributed_approach;
 
 import peersim.config.Configuration;
 
-/**
- *
- * @author sakina
- */
+
 public class DataItem {
     String id;
     Bounds [] bound;
