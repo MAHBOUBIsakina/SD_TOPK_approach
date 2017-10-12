4dd96aa0433a35a136a4387a143eb11cca966db6
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 14:19:14 +0200

    Update Step2Protocol.java

--- src/java/Distributed_topk_query_processing/Step2Protocol.java
+++ src/java/Distributed_topk_query_processing/Step2Protocol.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package distributed_approach;
 
 
-//import distributed_top_k.core.DataNode;
-//import distributed_top_k.core.MasterDataNode;
-//import distributed_top_k.core.Message1;
-//import distributed_top_k.core.Message3;
-//import distributed_top_k.core.Message3Value;
-//import distributed_top_k.core.SimulationController;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
@@ -25,10 +15,8 @@ import peersim.core.Node;
 import peersim.edsim.EDProtocol;
 import peersim.edsim.EDSimulator;
 import peersim.transport.Transport;
-/**
- *
- * @author sakina
- */
+
+
 public class Step2Protocol implements EDProtocol{
     
     
@@ -41,29 +29,16 @@ public class Step2Protocol implements EDProtocol{
     
     @Override
     public void processEvent(Node node, int pid, Object event) {
-        
-        
         MasterDataNode master = (MasterDataNode) Network.get(0);
-        
         if (event instanceof Message1) {
             System.err.println("le temps écoulé pour que les neouds recoivent le message de deuxième étape est " + CommonState.getTime());
-            //System.out.println("event = Message1");
             Message1 msg = (Message1) event;
             if(msg.getSender() != null) {
                 double begin = System.currentTimeMillis();
                 ArrayList result = ((DataNode)node).getDataHighThanTH(msg.getValue());
-                //System.out.println("###############th = "+msg.getValue());
-                //System.out.println("''''''''''''''''''''''''''''''''''''number of data that have minovl great than TH="+result.size());
                 double end = System.currentTimeMillis();
                 Message3 reply = new Message3(result, node, (end-begin));
-                
-                //reply.printMessage3();
-                
-                
                 ((Transport)node.getProtocol(FastConfig.getTransport(pid))).send(node, master, reply, pid);
-                System.out.println("----------------latency between "+ node.getIndex()+" and "+ master.getIndex()+ " is "+((Transport)node.getProtocol(FastConfig.getTransport(pid))).getLatency(node, master));
-                
-                System.out.println("Data sent from " + node.getIndex() + " to " + master.getIndex());
             }  
         
         }
@@ -71,7 +46,6 @@ public class Step2Protocol implements EDProtocol{
             if (event instanceof Message3) {
                 if(!(node instanceof MasterDataNode)) return;
                 Message3 msg = (Message3) event;
-                System.out.println("le temps de calcul pour le noeud "+ msg.getSender().getIndex()+ " est "+ msg.getCalcul_time());
                 if (msg.getCalcul_time()>master.getMax_time()) {
                     master.setMax_time(msg.getCalcul_time());
                 }
@@ -79,21 +53,13 @@ public class Step2Protocol implements EDProtocol{
                 master.putStep2Result((ArrayList)msg.getValue(), msg.getSender().getIndex());
                 double end = System.currentTimeMillis();
                 master.setProcess_time(master.getProcess_time()+(end-begin));
-                System.err.println("******************************** le temps de collecter les données est "+ (end-begin));
-                //master.printdataCollection();
-                if(master.getNbrResponses() == Network.size()) { // Everyone responded
-                    // Process data
-                    
+                if(master.getNbrResponses() == Network.size()) { 
                     Set topKCondidate;
                     try {
                         double begin1 = System.currentTimeMillis();
                         topKCondidate = master.getKCondidateSet();
-                        System.out.println("''''''''''''''''''''''''''''''''''''''''''''''the number of top-k candidate is "+topKCondidate.size());
                         double end1 = System.currentTimeMillis();
-                        System.err.println("//////////////////////////////////////////// le temps d'obtention des k condidate est "+ (end1-begin1));
                         master.setProcess_time(master.getProcess_time()+(end1-begin1));
-                        System.out.println("le temps maximal pris par le master est "+ master.getMax_time());
-                        System.out.println("le temps de la 2ere étape chez le serveur est "+(end1-begin) );
                         System.out.println("Everyone responded! process data and start Step3 ");
                         SimulationController controller = (SimulationController)Configuration.getInstance(Configuration.getNames("control")[0]);
                         
@@ -103,19 +69,10 @@ public class Step2Protocol implements EDProtocol{
                     } catch (IOException ex) {
                         Logger.getLogger(Step2Protocol.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    
-                    
-                    
-                    //this.startStep2(master);
-                    
-                    
-                    
-                    //this.startStep2(master);
-                        
-                        
+   
                 }
             }
-            //System.out.println("event = Message3");
+            
         }
         
         
