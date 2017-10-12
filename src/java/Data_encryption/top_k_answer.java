a2c501998a3e108acc30fb31b8b16399b179bb81
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 11:46:04 +0200

    Update top_k_answer.java

--- src/java/Data_encryption/top_k_answer.java
+++ src/java/Data_encryption/top_k_answer.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package data_encryption;
 
-//import proposition_1_quicksort_optimise.*;
-//import proposition_1_optimisee.*;
-//import proposition_1_hash_function_byte.*;
 
-/**
- *
- * @author smahboub
- */
 public class top_k_answer {
      public String dataID;
     public double PeriScoreinf;
