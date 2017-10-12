cc1add1c05324f714875a97dc2124569bde77191
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 13:44:04 +0200

    Update Message1.java

--- src/java/Distributed_topk_query_processing/Message1.java
+++ src/java/Distributed_topk_query_processing/Message1.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package distributed_approach;
 
 import peersim.core.Node;
 
-/**
- *
- * @author sakina
- */
+
 public class Message1 {
     final double value;
     
