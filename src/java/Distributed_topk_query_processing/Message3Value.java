e253ae71af209db770fd0ee200aa10bec64f528a
MAHBOUBIsakina <mahboubisakina@yahoo.com>
2017-10-12 13:46:46 +0200

    Update Message3Value.java

--- src/java/Distributed_topk_query_processing/Message3Value.java
+++ src/java/Distributed_topk_query_processing/Message3Value.java
@@ -1,15 +1,6 @@
 package distributed_approach;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author sakina
- */
 public class Message3Value {
     String id;
     double min_bound;
